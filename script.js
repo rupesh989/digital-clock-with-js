fd62b7b3463ab50b04169236530d1ae0c3291037
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-02-08 19:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+const time = document.getElementById("time");
+const timeformat = document.getElementById("timeformat");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setInterval(showTime, 1000);
+});
+
+const showTime = () => {
+  let date = new Date();
+
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let secs = date.getSeconds();
+
+  //   console.log("hours", hr, "minutes", min, "seconds", secs);
+  hr = hr < 10 ? 10`0${hr}` : hr;
+  min = min < 10 ? 10`0${min}` : min;
+  secs = secs < 10 ? 10`0${secs}` : secs;
+
+  time.innerHTML = ` ${hr}: ${min}:${secs}`;
+
+  timeformat.innerHTML = hr > 12 ? "PM" : "AM";
+};
